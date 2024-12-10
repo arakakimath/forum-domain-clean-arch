db617597ef79ea72f64629fc7d06fbda42624651
Matheus Arakaki <arakakimath@gmail.com>
2024-12-10 15:31:57 -0400

    delete-question-use-case

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -4,6 +4,12 @@ import { Question } from '@/domain/forum/enterprise/entities/question'
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
 
+  async findById(id: string): Promise<Question | null> {
+    const question = this.items.find((item) => item.id.toString() === id)
+
+    return question ?? null
+  }
+
   async findBySlug(slug: string) {
     const question = this.items.find((item) => item.slug.value === slug)
 
@@ -12,5 +18,11 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
   async create(question: Question): Promise<void> {
     this.items.push(question)
+  }
+
+  async delete(question: Question): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+
+    this.items.splice(itemIndex, 1)
   }
 }
