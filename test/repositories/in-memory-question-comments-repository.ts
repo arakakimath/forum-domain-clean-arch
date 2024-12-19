5eba43a8b7ceff704058e58b71e90721a858f0f7
Matheus Arakaki <arakakimath@gmail.com>
2024-12-19 15:09:34 -0400

    fetch-question-comments-use-case

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -2,14 +2,24 @@ import { QuestionCommentsRepository } from '@/domain/forum/application/repositor
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
 
 export class InMemoryQuestionCommentsRepository
-  implements QuestionCommentsRepository
-{
+  implements QuestionCommentsRepository {
   public items: QuestionComment[] = []
 
   async findById(id: string): Promise<QuestionComment | null> {
     const questionComment = this.items.find((item) => item.id.toString() === id)
 
     return questionComment ?? null
+  }
+
+  async findManyByQuestionId(
+    questionId: string,
+    { page }: PaginationParams,
+  ): Promise<QuestionComment[]> {
+    const questionComments = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return questionComments
   }
 
   async create(questionComment: QuestionComment): Promise<void> {
