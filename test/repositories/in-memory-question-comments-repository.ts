b92f76732f9262509139439c395cccc885a7a957
Matheus Arakaki <arakakimath@gmail.com>
2024-12-19 14:54:22 -0400

    delete-question-comment-use-case

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -6,7 +6,21 @@ export class InMemoryQuestionCommentsRepository
 {
   public items: QuestionComment[] = []
 
+  async findById(id: string): Promise<QuestionComment | null> {
+    const questioncomment = this.items.find((item) => item.id.toString() === id)
+
+    return questioncomment ?? null
+  }
+
   async create(questionComment: QuestionComment): Promise<void> {
     this.items.push(questionComment)
+  }
+
+  async delete(questionComment: QuestionComment): Promise<void> {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === questionComment.id,
+    )
+
+    this.items.splice(itemIndex, 1)
   }
 }
