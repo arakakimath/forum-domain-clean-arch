4ceba0d8028153c8b0a5ce445c813bed5a987cc5
Matheus Arakaki <arakakimath@gmail.com>
2024-12-19 14:34:15 -0400

    repository methods for past use cases

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
@@ -8,6 +9,17 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     const answer = this.items.find((item) => item.id.toString() === id)
 
     return answer ?? null
+  }
+
+  async findManyByQuestionId(
+    questionId: string,
+    { page }: PaginationParams,
+  ): Promise<Answer[]> {
+    const answers = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answers
   }
 
   async create(answer: Answer): Promise<void> {
